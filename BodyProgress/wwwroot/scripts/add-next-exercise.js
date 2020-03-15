381f005b7ca94654d714c0a850a48e3021735c9f
Patryk Binicki <pbinicki@gmail.com>
2020-03-15 19:43:08 +0100

    Changes in a pages design

---
+++ BodyProgress/wwwroot/scripts/add-next-exercise.js
@@ -0,0 +1,18 @@
+﻿const newItem = () => ({
+    exerciseId: null,
+    weight: 0,
+    reps: 0
+});
+new Vue({
+    el: '#create-training',
+    data() {
+        return {
+            planItems: [newItem()]
+        }
+    },
+    methods: {
+        addNext() {
+            this.planItems.push(newItem())
+        }
+    }
+})
